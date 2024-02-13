d5941e61592c9338f16db5578f9759c35e6ec1a2
luxmi1799 <luxmigupta1799@gmail.com>
2024-02-13 23:29:17 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:log_in/Authentication/splash.dart';
 import 'package:log_in/log_in.dart';
 
 void main() {
   runApp(const login());
+  // runApp(const login());
 }
 
 class login extends StatelessWidget {
@@ -18,10 +20,10 @@ class login extends StatelessWidget {
         pageTransitionsTheme: _removeTransitions(),
       ),
       debugShowCheckedModeBanner: false,
-      home: const log_in(),
+      home: const Splash(),
+      //home: const log_in(),
     );
   }
 }
 
 _removeTransitions() {}
-
