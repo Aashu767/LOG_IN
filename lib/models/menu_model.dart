cddda9c29d4ff1fc3d413e5017ad9f6b71f5c0b6
Luxmi Gupta <luxmi.gupta@192.168.1.20>
2024-02-18 22:20:43 +0530

    menumodel

---
+++ lib/models/menu_model.dart
@@ -0,0 +1,21 @@
+class MenuModel {
+  int? menuId;
+  String? menuName;
+  int? count;
+
+  MenuModel({this.menuId, this.menuName, this.count});
+
+  MenuModel.fromJson(Map<String, dynamic> json) {
+    menuId = json['Menu_Id'];
+    menuName = json['Menu_Name'];
+    count = json['Count'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['Menu_Id'] = this.menuId;
+    data['Menu_Name'] = this.menuName;
+    data['Count'] = this.count;
+    return data;
+  }
+}
