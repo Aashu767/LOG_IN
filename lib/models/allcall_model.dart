26704d77b15f2edac1a1dbf466971f4eb98678fa
Aashu767 <151607986+Aashu767@users.noreply.github.com>
2024-03-12 13:38:11 +0530

    commit

---
+++ lib/models/allcall_model.dart
@@ -0,0 +1,198 @@
+// ignore_for_file: non_constant_identifier_names
+
+library com.nds.model;
+
+class AllMyCallsModel {
+  late String ComplainNo;
+  late String COMPLAINTType;
+  late String Assignedby;
+  late String visitpreferenceDate;
+  late String CustomerName;
+  late String CustomerAddress;
+  late String CustomerEmail;
+  late String CustomerMobile;
+  late String ProductName;
+  late String Problem;
+  late String PurchaseDate;
+  late String WarrantyStatus;
+  late String Status;
+  late String CustomerId;
+  late String Name;
+  late String Rate;
+  late String Id;
+  late String Count;
+
+  String getQuantity() {
+    return Quantity;
+  }
+
+  void setQuantity(String quantity) {
+    Quantity = quantity;
+  }
+
+  late String Quantity;
+  late Integer dashboardBGImage;
+  late Integer dashboardImage;
+
+  String getComplainNo() {
+    return ComplainNo;
+  }
+
+  void setComplainNo(String ComplainNo) {
+    this.ComplainNo = ComplainNo;
+  }
+
+  String getCOMPLAINTType() {
+    return COMPLAINTType;
+  }
+
+  void setCOMPLAINTType(String COMPLAINTType) {
+    this.COMPLAINTType = COMPLAINTType;
+  }
+
+  String getAssignedby() {
+    return Assignedby;
+  }
+
+  void setAssignedby(String Assignedby) {
+    this.Assignedby = Assignedby;
+  }
+
+  String getvisitpreferenceDate() {
+    return visitpreferenceDate;
+  }
+
+  void setvisitpreferenceDate(String visitpreferenceDate) {
+    this.visitpreferenceDate = visitpreferenceDate;
+  }
+
+  String getCustomerName() {
+    return CustomerName;
+  }
+
+  void setCustomerName(String CustomerName) {
+    this.CustomerName = CustomerName;
+  }
+
+  String getCustomerAddress() {
+    return CustomerAddress;
+  }
+
+  void setCustomerAddress(String CustomerAddress) {
+    this.CustomerAddress = CustomerAddress;
+  }
+
+  String getCustomerEmail() {
+    return CustomerEmail;
+  }
+
+  void setCustomerEmail(String CustomerEmail) {
+    this.CustomerEmail = CustomerEmail;
+  }
+
+  String getCustomerMobile() {
+    return CustomerMobile;
+  }
+
+  void setCustomerMobile(String CustomerMobile) {
+    this.CustomerMobile = CustomerMobile;
+  }
+
+  String getProductName() {
+    return ProductName;
+  }
+
+  void setProductName(String ProductName) {
+    this.ProductName = ProductName;
+  }
+
+  String getProblem() {
+    return Problem;
+  }
+
+  void setProblem(String Problem) {
+    this.Problem = Problem;
+  }
+
+  String getPurchaseDate() {
+    return PurchaseDate;
+  }
+
+  void setPurchaseDate(String PurchaseDate) {
+    this.PurchaseDate = PurchaseDate;
+  }
+
+  String getWarrantyStatus() {
+    return WarrantyStatus;
+  }
+
+  void setWarrantyStatus(String WarrantyStatus) {
+    this.WarrantyStatus = WarrantyStatus;
+  }
+
+  String getStatus() {
+    return Status;
+  }
+
+  void setStatus(String Status) {
+    this.Status = Status;
+  }
+
+  String getCustomerId() {
+    return CustomerId;
+  }
+
+  void setCustomerId(String CustomerId) {
+    this.CustomerId = CustomerId;
+  }
+
+  String getName() {
+    return Name;
+  }
+
+  void setName(String Name) {
+    this.Name = Name;
+  }
+
+  String getRate() {
+    return Rate;
+  }
+
+  void setRate(String Rate) {
+    this.Rate = Rate;
+  }
+
+  String getId() {
+    return Id;
+  }
+
+  void setId(String Id) {
+    this.Id = Id;
+  }
+
+  String getCount() {
+    return Count;
+  }
+
+  void setCount(String Count) {
+    this.Count = Count;
+  }
+
+  Integer getdashboardBGImage() {
+    return dashboardBGImage;
+  }
+
+  void setdashboardBGImage(Integer dashboardBGImage) {
+    this.dashboardBGImage = dashboardBGImage;
+  }
+
+  Integer getdashboardImage() {
+    return dashboardImage;
+  }
+
+  void setdashboardImage(Integer dashboardImage) {
+    this.dashboardImage = dashboardImage;
+  }
+}
+
+class Integer {}
---
+++ lib/utils/GlobalUrl.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: file_names, non_constant_identifier_names, unused_import
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class GlobalURl {
+  static String localnew =
+      "http://nds1.nippondata.com/ServiceWebApi/Service.asmx";
+  static String Base_URL = localnew;
+  static String MyallCompleted = "ws_all_complete_call?";
+  static String Ws_Validate_Login = "Ws_Validate_Login?";
+  static String Ws_Day_Check_In = "Ws_Day_Check_In?";
+  static String Ws_Day_Check_In_With_Reading = "Ws_Day_Check_In_With_Reading?";
+  static String Ws_Day_Check_Out = "Ws_Day_Check_Out?";
+  static String Ws_Day_Check_Out_With_Reading =
+      "Ws_Day_Check_Out_With_Reading?";
+  static String Ws_Leave_Request = "Ws_Leave_Request?";
+  static String Ws_Day_Feedback = "Ws_Day_Feedback?";
+  static String Ws_Get_All_MenuLinks = "Ws_Get_All_MenuLinks?";
+  static String Ws_Get_All_MenuLinks_V2 = "Ws_Get_All_MenuLinks_V2?";
+  static String Get_All_MenuLinks_SR = "Get_All_MenuLinks_SR?";
+  static String Ws_Get_All_ComplaintsType = "Ws_Get_All_ComplaintsType?";
+  static String Ws_Get_All_ComplaintsType_v2 = "Ws_Get_All_ComplaintsType_V2?";
+  static String Ws_Get_All_ComplaintsType_SR = "Ws_Get_All_ComplaintsType_SR?";
+  static String Ws_Get_All_ComplaintsList = "Ws_Get_All_ComplaintsList?";
+  static String Ws_Get_All_ComplaintsList_v2 = "Ws_Get_All_ComplaintsList_V2?";
+  static String Ws_Get_All_ComplaintsList_SR = "Ws_Get_All_ComplaintsList_SR?";
+  static String Ws_Get_Complaint_BasedOnComplaintNo_SR =
+      "Ws_Get_Complaint_BasedOnComplaintNo_SR?";
+  static String Ws_Get_All_DropDownValue = "Ws_Get_All_DropDownValue?";
+  static String Ws_Get_All_DropDownValueSR = "Ws_Get_All_DropDownValueSR?";
+  static String WS_Update_complaints = "WS_Update_complaints?";
+  static String WS_Update_complaints_V2 = "WS_Update_complaints_V2?";
+  static String WS_Update_complaints_V3 = "WS_Update_complaints_V3?";
+  static String WS_Update_complaints_SR = "WS_Update_complaints_SR?";
+  static String Ws_Tenker_Request_List = "Ws_Tenker_Request_List?";
+  static String Ws_Tenker_Request_List_V2 = "Ws_Tenker_Request_List_V2?";
+  static String WS_Tenker_Request_Update = "WS_Tenker_Request_Update?";
+  static String WS_Tenker_Request_Update_V2 = "WS_Tenker_Request_Update_V2?";
+  static String Ws_Get_All_Bind_subordinate = "Ws_Get_All_Bind_subordinate?";
+  static String WS_UPdate_Complaint_Assign = "WS_UPdate_Complaint_Assign?";
+  static String Search_ComplaintsList = "Search_ComplaintsList?";
+  static String Ws_Tenker_Request_StatusWise_Count =
+      "Ws_Tenker_Request_StatusWise_Count?";
+  static String Ws_Tenker_Request_StatusWise_Count_v2 =
+      "Ws_Tenker_Request_StatusWise_Count_V2?";
+  static String Ws_Get_All_Zones = "Ws_Get_All_Zones?";
+  static String Ws_Get_All_Zones_v2 = "Ws_Get_All_Zones_V2?";
+  static String Ws_Get_All_Zones_SR = "Ws_Get_All_Zones_SR?";
+  static String Ws_Get_Version = "Ws_Get_Version?";
+}
